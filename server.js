2123959cd8ddd11023ea3a3e3beefd5b22ba9cc0
mse2321 <mse233@gmail.com>
2015-07-07 00:24:24 -0400

    updated server.js

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 // Load module dependencies
 var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+//var url = 'mongodb://localhost:27017/test';
+
+/*
+//MongoDB Connect
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server.");
+  db.close();
+});
+*/
 
 // Create Express app
 var app = express();
