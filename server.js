f47707d866aee0edcc33e1a300d2801542b78471
mse2321 <mse233@gmail.com>
2015-07-07 00:01:28 -0400

    made updates to server file

--- server.js
+++ server.js
@@ -1,18 +1,8 @@
 // Load module dependencies
 var express = require('express');
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
 
 // Create Express app
 var app = express();
-
-// MongoDB Connect
-var url = 'mongodb://localhost:27017/test';
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server.");
-  db.close();
-});
 
 // Set the way to serve static content
 app.use(express.static('public'));
